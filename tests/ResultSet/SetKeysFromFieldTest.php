ba6dae79f7a04166e95d1f24c83a16bfd02d00e0
Barnaby Norman <barnabynorman@users.noreply.github.com>
2020-02-21 06:00:27 +0100

    Fix where class definitions missing some fields - added blank data

--- tests/ResultSet/SetKeysFromFieldTest.php
+++ tests/ResultSet/SetKeysFromFieldTest.php
@@ -5,23 +5,22 @@ namespace Tests\ResultSet;
 use ResultSet\ResultSet;
 use Tests\AbstractTestCase;
 
-
 class SetKeysFromFieldTest extends AbstractTestCase {
 
   public function testKeyOrderReturned()
   {
     $items = [];
-    $items[] = new GroceryItem([ "id" => "1", "name" => "Orange" ]);
-    $items[] = new GroceryItem([ "id" => "2", "name" => "Apple" ]);
-    $items[] = new GroceryItem([ "id" => "3", "name" => "Pear" ]);
-    $items[] = new GroceryItem([ "id" => "4", "name" => "Potato" ]);
-    $items[] = new GroceryItem([ "id" => "5", "name" => "Pepper" ]);
-    $items[] = new GroceryItem([ "id" => "6", "name" => "Tomato" ]);
-    $items[] = new GroceryItem([ "id" => "7", "name" => "Celery" ]);
-    $items[] = new GroceryItem([ "id" => "8", "name" => "Kipper" ]);
-    $items[] = new GroceryItem([ "id" => "9", "name" => "Baked beans" ]);
-    $items[] = new GroceryItem([ "id" => "10", "name" => "Ketchup" ]);
-    $items[] = new GroceryItem([ "id" => "11", "name" => "Beer" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "1", "name" => "Orange" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "2", "name" => "Apple" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "3", "name" => "Pear" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "4", "name" => "Potato" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "5", "name" => "Pepper" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "6", "name" => "Tomato" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "7", "name" => "Celery" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "8", "name" => "Kipper" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "9", "name" => "Baked beans" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "10", "name" => "Ketchup" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "11", "name" => "Beer" ]);
 
     $itemsRs = ResultSet::getInstance($items);
 
@@ -41,9 +40,9 @@ class SetKeysFromFieldTest extends AbstractTestCase {
   public function testWithSpaceInValue()
   {
     $items = [];
-    $items[] = new GroceryItem([ "id" => "9", "name" => "Baked beans" ]);
-    $items[] = new GroceryItem([ "id" => "10", "name" => "Ketchup" ]);
-    $items[] = new GroceryItem([ "id" => "11", "name" => "Beer" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "9", "name" => "Baked beans" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "10", "name" => "Ketchup" ]);
+    $items[] = new GroceryItem([ "type_id" => "", "colour" => "", "id" => "11", "name" => "Beer" ]);
 
     $itemsRs = ResultSet::getInstance($items);
 
