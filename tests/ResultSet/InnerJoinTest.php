36913e1dde8264353cd1d1e23346bbf506a52375
Barnaby Norman <barnabynorman@users.noreply.github.com>
2024-06-29 17:43:22 +0100

    Fix test where missing keys and colour

--- tests/ResultSet/InnerJoinTest.php
+++ tests/ResultSet/InnerJoinTest.php
@@ -14,7 +14,13 @@ class InnerJoinTest extends AbstractTestCase {
     $groceryTypes[] = new GroceryType(['type_id' => 7, 'name' => 'Pet food']);
 
     $groceryItems = TestData::getItems();
-    $groceryItems[] = new GroceryItem(11, 'Cheese', 8);
+    $groceryItems[] = new GroceryItem([
+      'id' => 11,
+      'name' => 'Cheese',
+      'type_id' => 8,
+      'colour' => 'topaz',
+    ]);
+
     $groceryItemsRs = new ResultSet($groceryItems);
 
     $result = $groceryItemsRs->innerJoin($groceryTypes, 'type', ['typeId' => 'id']);
